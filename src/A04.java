513bbfdbe70e3a7e3cf7f212c5dc177eeeb11732
Sevil Oztrk <svloztrk9@gmail.com>
2022-11-06 12:35:49 +0300

    aciklamalar eklendi

--- src/A04.java
+++ src/A04.java
@@ -1,6 +1,19 @@
 public class A04 {
     public static void main(String[] args) {
 
+        // bir projeyi ilk defa olusturdugumuzda
+        /*
+        1- commit olustur
+        2- uzak masaustunde bir repo olustur
+        3- githubda istenen son 3 satiri copy yapip projeye paste yapip calistir.
 
+        bir projeyi bir kere uzak masaüstü ile iliskilendirince sonra yapacağımz her degisiklik icin
+        1- git add . yeni yapilan degisiklikleri git'e ekler
+        2- git commit -m= degisiklikleri isleyerek version olusturur
+        3- git push= son versiyonu uzak masaüstüne yollar
+
+
+
+         */
     }
 }
